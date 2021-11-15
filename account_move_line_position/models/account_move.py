39a2a602c780adf14524ccdd82f8eb07201d12f9
Janik von Rotz <login@janikvonrotz.ch>
2021-11-15 09:52:20 +0100

    fetch positions with line links

--- account_move_line_position/models/account_move.py
+++ account_move_line_position/models/account_move.py
@@ -11,9 +11,13 @@ class AccountMoveLine(models.Model):
     def _compute_get_position(self):
         _logger.warning(self)
         for rec in self:
-            if rec.move_id.sale_order_id:
-                rec.position = rec.move_id.sale_order_id.get_position(rec.product_id, rec.quantity)
-            elif rec.purchase_order_id:
-                rec.position = rec.purchase_order_id.get_position(rec.product_id, rec.quantity)
+            if rec.sale_line_ids:
+                rec.position = rec.sale_line_ids[0].position
+            # if rec.move_id.sale_order_id:
+            #     rec.position = rec.move_id.sale_order_id.get_position(rec.product_id, rec.quantity)
+            elif rec.purchase_line_id:
+                rec.position = rec.purchase_line_id.position
+            # elif rec.purchase_order_id:
+            #     rec.position = rec.purchase_order_id.get_position(rec.product_id, rec.quantity)
             else:
                 rec.position = 0
