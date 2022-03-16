bb3130dab92839a5979a7d586cadf1902b42b216
Janik von Rotz <login@janikvonrotz.ch>
2022-03-16 10:00:22 +0100

    ensure field is stored

--- account_move_reconciled_date/models/account_move.py
+++ account_move_reconciled_date/models/account_move.py
@@ -6,8 +6,9 @@ _logger = logging.getLogger(__name__)
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    reconcile_date = fields.Date(compute='_compute_get_reconcile_date')
+    reconcile_date = fields.Date(compute='_compute_get_reconcile_date', store=True)
 
+    @api.depends('payment_state')
     def _compute_get_reconcile_date(self):
         for move in self:
             reconcilation_dates = []
