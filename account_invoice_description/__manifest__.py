c0df93a9949a7c669a6ea161100ed000e42b3f79
janikvonrotz <contact@janikvonrotz.ch>
2022-04-11 14:24:44 +0200

    update readmes

--- account_invoice_description/__manifest__.py
+++ account_invoice_description/__manifest__.py
@@ -1,24 +1,16 @@
 {
-    'name': "Account Invoice Description",
-
-    'summary': """
+    "name": "Account Invoice Description",
+    "summary": """
         Add description to invoice form and report.
     """,
-
-    'author': "Mint System GmbH, Odoo Community Association (OCA)",
-    'website': "https://www.mint-system.ch",
-    'category': 'Accounting',
-    'version': '14.0.1.0.1',
-    'license': 'AGPL-3',
-
-    'depends': ['account'],
-
-    'data': [
-        'views/report_invoice_document.xml',
-        'views/view_move_form.xml',
-    ],
-
-    'installable': True,
-    'application': False,
+    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "website": "https://www.mint-system.ch",
+    "category": "Accounting",
+    "version": "14.0.1.0.1",
+    "license": "AGPL-3",
+    "depends": ["account"],
+    "data": ["views/report_invoice_document.xml", "views/view_move_form.xml"],
+    "installable": True,
+    "application": False,
     "images": ["images/screen.png"],
 }
--- account_invoice_detail/__manifest__.py
+++ account_invoice_detail/__manifest__.py
@@ -1,23 +1,16 @@
 {
-    'name': "Account Invoice Detail",
-
-    'summary': """
+    "name": "Account Invoice Detail",
+    "summary": """
         Add field salesperson to invoice.
     """,
-
-    'author': "Mint System GmbH, Odoo Community Association (OCA)",
-    'website': "https://www.mint-system.ch",
-    'category': 'Accounting',
-    'version': '14.0.1.0.1',
-    'license': 'AGPL-3',
-
-    'depends': ['account'],
-
-    'data': [
-        'views/account_report_invoice_document.xml',
-    ],
-
-    'installable': True,
-    'application': False,
+    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "website": "https://www.mint-system.ch",
+    "category": "Accounting",
+    "version": "14.0.1.0.1",
+    "license": "AGPL-3",
+    "depends": ["account"],
+    "data": ["views/account_report_invoice_document.xml"],
+    "installable": True,
+    "application": False,
     "images": ["images/screen.png"],
 }
