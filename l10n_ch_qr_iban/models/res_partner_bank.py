9e25e17af60e45f2bfd2931c37b929c29e310910
janikvonrotz <contact@janikvonrotz.ch>
2022-06-07 15:11:29 +0200

    [l10n_ch_qr_iban] bump

--- l10n_ch_qr_iban/models/res_partner_bank.py
+++ l10n_ch_qr_iban/models/res_partner_bank.py
@@ -6,8 +6,14 @@ _logger = logging.getLogger(__name__)
 class ResPartnerBank(models.Model):
     _inherit = 'res.partner.bank'
     
-    def _check_qr_iban_range(self, iban):
-        """OVERWRITE: All ibans are valid qr ibans"""
-        if not iban or len(iban) < 9:
-            return False
-        return True
+    # def _check_qr_iban_range(self, iban):
+    #     """OVERWRITE: All ibans are valid qr ibans"""
+    #     if not iban or len(iban) < 9:
+    #         return False
+    #     return True
+
+    @api.model
+    def _compute_name_from_postal_number(self, partner_name, postal_number):
+        """Do not prefix account name with 'ISR'."""
+        res = super()._compute_name_from_postal_number(partner_name, postal_number)
+        return res.replace('ISR ','')
