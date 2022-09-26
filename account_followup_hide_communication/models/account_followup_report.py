b51e59a017a64e0dd2a40f972dbccdd0d318ee7a
janikvonrotz <contact@janikvonrotz.ch>
2022-09-26 17:03:06 +0200

    [account_followup_hide_communication] update

--- account_followup_hide_communication/models/account_followup_report.py
+++ account_followup_hide_communication/models/account_followup_report.py
@@ -12,3 +12,12 @@ class AccountFollowupReport(models.AbstractModel):
             if item.get('name') and item.get('name') == _('Communication'):
                 res[index]['style'] = 'display:none;'
         return res
+
+    def _get_lines(self, options, line_id=None):
+        res = super(AccountFollowupReport , self)._get_lines(options, line_id)
+        for index, item in enumerate(res):
+            columns = item.get('columns')      
+            if columns:
+                columns.pop(3)
+                res[index]['columns'] = columns
+        return res
