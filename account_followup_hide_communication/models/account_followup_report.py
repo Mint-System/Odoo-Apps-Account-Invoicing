b1fbb9b58a37a30b0be5715d0971a2989a160274
janikvonrotz <contact@janikvonrotz.ch>
2022-09-26 17:11:35 +0200

    [account_followup_hide_communication] fix selector

--- account_followup_hide_communication/models/account_followup_report.py
+++ account_followup_hide_communication/models/account_followup_report.py
@@ -10,14 +10,16 @@ class AccountFollowupReport(models.AbstractModel):
         res = super(AccountFollowupReport , self)._get_columns_name(options)
         for index, item in enumerate(res):
             if item.get('name') and item.get('name') == _('Communication'):
-                res[index]['style'] = 'display:none;'
+                res.pop(index)
         return res
 
     def _get_lines(self, options, line_id=None):
         res = super(AccountFollowupReport , self)._get_lines(options, line_id)
+        _logger.warning(res)
         for index, item in enumerate(res):
             columns = item.get('columns')      
             if columns:
                 columns.pop(3)
                 res[index]['columns'] = columns
+        _logger.warning(res)
         return res
