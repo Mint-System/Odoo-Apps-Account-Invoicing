465721bcaee8f9d0e90956afd8b6b803101473cb
janikvonrotz <contact@janikvonrotz.ch>
2022-11-28 14:03:12 +0100

    [account_invoice_sale_partner] migrate

---
+++ account_invoice_sale_partner/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Account Invoice Sale Partner",
+    "summary": """
+        Set sale order contact on invoice.
+    """,
+    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "website": "https://www.mint-system.ch",
+    "category": "Invoicing",
+    "version": "15.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["account", "partner_type_sale"],
+    "data": ["views/account.xml"],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "images": ["images/screen.png"],
+}
