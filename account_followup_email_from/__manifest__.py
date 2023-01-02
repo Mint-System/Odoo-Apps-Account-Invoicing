e6c94210052b3c8a9425b64c90ca20735a14ef60
janikvonrotz <contact@janikvonrotz.ch>
2023-01-02 19:54:37 +0100

    [account_followup_email_from] init

---
+++ account_followup_email_from/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Account Followup Email From",
+    "summary": """
+        Send followup e-mail from specific address.
+    """,
+    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "website": "https://www.mint-system.ch",
+    "category": "Invoicing",
+    "version": "15.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["account_followup"],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "images": ["images/screen.png"],
+}
