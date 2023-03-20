d6aa80d03e6f784a60d75dcec1b4975e89633196
Mitchel Admin <mitcheladmin@example.com>
2023-03-20 13:51:22 +0100

    [account_invoice_multiline_ref] hide ref in displayname

--- account_invoice_multiline_ref/models/account_move.py
+++ account_invoice_multiline_ref/models/account_move.py
@@ -1,12 +1,16 @@
-from odoo import models, fields
+from odoo import models, fields, api
 import logging
 _logger = logging.getLogger(__name__)
-
+from odoo.tools.misc import format_date
 
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
     ref = fields.Text()
+
+    def _get_move_display_name(self, show_ref=False):
+        out_types = ['out_invoice', 'out_refund', 'out_receipt']
+        return super()._get_move_display_name(show_ref=(self.move_type not in out_types))
 
 class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
