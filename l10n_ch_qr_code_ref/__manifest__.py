917c286afa853352da4b62e4a104d4506468954e
Janik von Rotz <login@janikvonrotz.ch>
2023-04-24 09:24:47 +0200

    [l10n_ch_qr_code_ref] add template

--- l10n_ch_qr_code_ref/__manifest__.py
+++ l10n_ch_qr_code_ref/__manifest__.py
@@ -9,6 +9,7 @@
     "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["l10n_ch"],
+    "data": ["views/l10n_ch.xml"],
     "installable": True,
     "application": False,
     "auto_install": False,
--- l10n_ch_qr_code_ref/models/swissqr_report.py
+++ l10n_ch_qr_code_ref/models/swissqr_report.py
@@ -6,6 +6,7 @@ class ReportSwissQR(models.AbstractModel):
     @api.model
     def _get_report_values(self, docids, data=None):
         res = super()._get_report_values(docids, data)
+        docs = self.env['account.move'].browse(docids)
         for invoice in docs:
             res[invoice.id] = invoice.partner_bank_id.build_qr_code_base64(invoice.amount_residual, invoice.name, invoice.payment_reference, invoice.currency_id, invoice.partner_id, qr_method='ch_qr', silent_errors=False)
         return res
