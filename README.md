393809a1aac3a47bfa3b53b8b858944fb5744c7d
Janik von Rotz <login@janikvonrotz.ch>
2023-10-17 09:15:36 +0200

    init 16.0 branch

---
+++ README.md
@@ -0,0 +1,17 @@
+# Odoo Apps: Account Invoicing
+
+Collection of account and invoicing model related modules.
+
+## Usage
+
+Clone module into Odoo addon directory.
+
+```bash
+git clone git@github.com:mint-system/odoo-apps-account-invoicing.git ./addons/account_invoicing
+```
+
+## Available modules
+
+| Module | Summary |
+| --- | --- |
+| [account_invoice_select_bank](account_invoice_select_bank) |         Select bank by matching currency of invoice. |
---
+++ account_invoice_select_bank/models/account_move.py
@@ -0,0 +1,30 @@
+import logging
+
+from odoo import api, models
+
+_logger = logging.getLogger(__name__)
+
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    @api.onchange("currency_id")
+    def _onchange_currency_id(self):
+        self._compute_partner_bank_id()
+
+    @api.depends("bank_partner_id")
+    def _compute_partner_bank_id(self):
+        res = super()._compute_partner_bank_id()
+        for move in self:
+            bank_ids = move.bank_partner_id.bank_ids.filtered(
+                lambda bank: bank.company_id is False
+                or bank.company_id == move.company_id
+            )
+            bank_currency_ids = bank_ids.filtered(
+                lambda bank: bank.currency_id == move.currency_id
+            )
+            if bank_currency_ids:
+                move.partner_bank_id = bank_currency_ids[0]
+            else:
+                move.partner_bank_id = bank_ids and bank_ids[0]
+        return res
