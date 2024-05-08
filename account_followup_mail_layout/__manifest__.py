0045e939c63fb4e6f440446c6ef12942b1e6d1fc
Janik von Rotz <login@janikvonrotz.ch>
2024-05-08 10:46:01 +0200

    fix: remove view

--- account_followup_mail_layout/__manifest__.py
+++ account_followup_mail_layout/__manifest__.py
@@ -9,7 +9,6 @@
     "version": "16.0.1.0.0",
     "license": "OPL-1",
     "depends": ["account_followup"],
-    "data": ["views/res_partner.xml"],
     "installable": True,
     "application": False,
     "auto_install": False,
