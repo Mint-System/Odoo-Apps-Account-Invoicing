f2af278dafff2e8a28137e8ef96376138e962af5
Laszlo Krajnikovszkij <laszlo.lk@protonmail.com>
2024-06-18 15:02:16 +0200

    chore: link module, update readme

--- account_invoice_carrier/models/account_move.py
+++ account_invoice_carrier/models/account_move.py
@@ -1,12 +1,17 @@
-from odoo import api, fields, models, _
+from odoo import api, fields, models
 
 
 class AccountMove(models.Model):
-    _inherit = 'account.move'
+    _inherit = "account.move"
 
-    carrier_id = fields.Many2one('delivery.carrier', string="Delivery Method", domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]", help="Fill this field if you plan to invoice the shipping based on picking.")
+    carrier_id = fields.Many2one(
+        "delivery.carrier",
+        string="Delivery Method",
+        domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]",
+        help="Fill this field if you plan to invoice the shipping based on picking.",
+    )
 
-    @api.onchange('partner_id')
+    @api.onchange("partner_id")
     def _onchange_carrier_id(self):
         for move in self.filtered(lambda m: not m.carrier_id):
             move.carrier_id = move.partner_id.property_delivery_carrier_id
