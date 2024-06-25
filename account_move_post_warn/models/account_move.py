3e2fff1a794467692f1639b8e232b00effb8b2c4
Laszlo Krajnikovszkij <laszlo.lk@protonmail.com>
2024-06-25 14:32:56 +0200

    chore: update readme, lint module

--- account_move_post_warn/models/account_move.py
+++ account_move_post_warn/models/account_move.py
@@ -1,10 +1,12 @@
-from odoo import api, fields, models, _
 import logging
+
+from odoo import _, models
+
 _logger = logging.getLogger(__name__)
 
 
 class AccountMove(models.Model):
-    _inherit = 'account.move'
+    _inherit = "account.move"
 
     def action_post(self):
         """Show warning if one is set."""
@@ -13,25 +15,31 @@ class AccountMove(models.Model):
         warning = {}
         if self.partner_id:
             p = self.partner_id
-            if p.invoice_warn == 'no-message' and p.parent_id:
+            if p.invoice_warn == "no-message" and p.parent_id:
                 p = p.parent_id
-            if p.invoice_warn and p.invoice_warn != 'no-message':
+            if p.invoice_warn and p.invoice_warn != "no-message":
                 # Block if partner only has warning but parent company is blocked
-                if p.invoice_warn != 'block' and p.parent_id and p.parent_id.invoice_warn == 'block':
+                if (
+                    p.invoice_warn != "block"
+                    and p.parent_id
+                    and p.parent_id.invoice_warn == "block"
+                ):
                     p = p.parent_id
                 warning = {
-                    'title': _("Warning for %s", p.name),
-                    'message': p.invoice_warn_msg
+                    "title": _("Warning for %s", p.name),
+                    "message": p.invoice_warn_msg,
                 }
-                if p.invoice_warn == 'block':
+                if p.invoice_warn == "block":
                     self.partner_id = False
-                    warning = {'warning': warning}
+                    warning = {"warning": warning}
 
         if warning:
-            odoobot = self.env.ref('base.partner_root')
-            self.message_post(body=warning['title']+': '+warning['message'],
-                              message_type='comment',
-                              subtype_xmlid='mail.mt_note',
-                              author_id=odoobot.id)
-        
+            odoobot = self.env.ref("base.partner_root")
+            self.message_post(
+                body=warning["title"] + ": " + warning["message"],
+                message_type="comment",
+                subtype_xmlid="mail.mt_note",
+                author_id=odoobot.id,
+            )
+
         return super().action_post()
