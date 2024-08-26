94c21247f278159294d4b9203be23283a30abf09
Laszlo Krajnikovszkij <laszlo@laszlo.is>
2024-08-26 11:56:50 +0200

    feat: upgrade module

---
+++ l10n_ch_disable_default_reports/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Switzerland Disable Default Reports",
+    "summary": """
+        Disable QR and ISR report generation.
+    """,
+    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "website": "https://www.mint-system.ch",
+    "category": "Accounting",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["l10n_ch"],
+    "data": ["views/account_move.xml"],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "images": ["images/screen.png"],
+}
---
+++ l10n_ch_disable_default_reports/models/account_move.py
@@ -0,0 +1,21 @@
+import logging
+
+from odoo import fields, models
+
+_logger = logging.getLogger(__name__)
+
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    enable_ch_qr_code = fields.Boolean(
+        default=True,
+    )
+
+    def _compute_l10n_ch_is_qr_valid(self):
+        """Set QR code validity based on enable_ch_qr_code field."""
+        result = super()._compute_l10n_ch_is_qr_valid()
+        for move in self:
+            if not move.enable_ch_qr_code:
+                move.l10n_ch_is_qr_valid = False
+        return result
