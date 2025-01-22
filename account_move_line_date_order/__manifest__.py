b3411b63bf27cd35b9576ab88b55bd892e98abf9
Janik von Rotz <login@janikvonrotz.ch>
2025-01-22 10:21:15 +0100

    feat: init account_move_line_date_order

---
+++ account_move_line_date_order/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "Account Move Line Date Order",
+    "summary": """
+        Module summary.
+    """,
+    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "website": "https://www.mint-system.ch",
+    "category": "Account Invoicing",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["sale"],
+    "data": [
+        "views/account_move_line_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "images": ["images/screen.png"],
+}
