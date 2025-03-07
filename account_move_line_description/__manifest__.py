30dc53b60d82686a7ba847edb336c47c6da693a3
Ulrich Kilian <uk@science-and-more.de>
2025-03-07 13:23:02 +0100

    feat: migrate account_move_line_description

---
+++ account_move_line_description/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Account Move Line Description",
+    "summary": """
+        Create invoice line description without product default code.
+    """,
+    "author": "Mint System GmbH",
+    "website": "https://www.mint-system.ch",
+    "category": "Invoicing",
+    "version": "17.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["account"],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "images": ["images/screen.png"],
+}
