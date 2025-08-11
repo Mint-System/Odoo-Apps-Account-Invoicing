ed6e83729c92e022d8044a8cbb673272d658a207
Ulrich Kilian <uk@science-and-more.de>
2025-08-11 11:49:25 +0200

    fix(account_currency_communication): add Switzerland option to communication standard on currencies

--- account_currency_communication/models/res_currency.py
+++ account_currency_communication/models/res_currency.py
@@ -15,4 +15,4 @@ class ResCurrency(models.Model):
         return 'odoo'
 
     invoice_reference_type = fields.Selection(string='Communication Type', required=True, selection=[('none', 'Open'), ('partner', 'Based on Customer'), ('invoice', 'Based on Invoice')], default='invoice', help='You can set here the default communication that will appear on customer invoices, once validated, to help the customer to refer to that particular invoice when making the payment.')
-    invoice_reference_model = fields.Selection(string='Communication Standard', required=True, selection=[('odoo', 'Odoo'), ('euro', 'European')], default=_default_invoice_reference_model, help="You can choose different models for each type of reference. The default one is the Odoo reference.")
+    invoice_reference_model = fields.Selection(string='Communication Standard', required=True, selection=[('odoo', 'Odoo'), ('euro', 'European'), ('ch', 'Switzerland')], default=_default_invoice_reference_model, help="You can choose different models for each type of reference. The default one is the Odoo reference.")
