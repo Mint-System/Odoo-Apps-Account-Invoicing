520b07df53305af3dcb887e70a745441026f9892
Kurt Gisler <kurt.gisler@mint-system.ch>
2026-08-21 15:29:15 +0200

    fix(account_move_post_warn): fix mutli value behaviour of action

--- account_move_post_warn/models/account_move.py
+++ account_move_post_warn/models/account_move.py
@@ -12,34 +12,35 @@ class AccountMove(models.Model):
         """Show warning if one is set."""
 
         # Copied from odoo/addons/account/models/account_move.py
-        warning = {}
-        if self.partner_id:
-            p = self.partner_id
-            if p.invoice_warn == "no-message" and p.parent_id:
-                p = p.parent_id
-            if p.invoice_warn and p.invoice_warn != "no-message":
-                # Block if partner only has warning but parent company is blocked
-                if (
-                    p.invoice_warn != "block"
-                    and p.parent_id
-                    and p.parent_id.invoice_warn == "block"
-                ):
+        for move in self:
+            warning = {}
+            if move.partner_id:
+                p = move.partner_id
+                if p.invoice_warn == "no-message" and p.parent_id:
                     p = p.parent_id
-                warning = {
-                    "title": _("Warning for %s", p.name),
-                    "message": p.invoice_warn_msg,
-                }
-                if p.invoice_warn == "block":
-                    self.partner_id = False
-                    warning = {"warning": warning}
+                if p.invoice_warn and p.invoice_warn != "no-message":
+                    # Block if partner only has warning but parent company is blocked
+                    if (
+                        p.invoice_warn != "block"
+                        and p.parent_id
+                        and p.parent_id.invoice_warn == "block"
+                    ):
+                        p = p.parent_id
+                    warning = {
+                        "title": _("Warning for %s", p.name),
+                        "message": p.invoice_warn_msg,
+                    }
+                    if p.invoice_warn == "block":
+                        move.partner_id = False
+                        warning = {"warning": warning}
 
-        if warning:
-            odoobot = self.env.ref("base.partner_root")
-            self.message_post(
-                body=warning["title"] + ": " + warning["message"],
-                message_type="comment",
-                subtype_xmlid="mail.mt_note",
-                author_id=odoobot.id,
-            )
+            if warning:
+                odoobot = move.env.ref("base.partner_root")
+                move.message_post(
+                    body=warning["title"] + ": " + warning["message"],
+                    message_type="comment",
+                    subtype_xmlid="mail.mt_note",
+                    author_id=odoobot.id,
+                )
 
         return super().action_post()
