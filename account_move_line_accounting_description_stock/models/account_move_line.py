48d3247f1eedeaeee68f57374811468322ccffb4
Ulrich Kilian <uk@sdcience-and-more.de>
2026-09-11 11:41:42 +0200

    feat(account_move_line_accounting_description_stock): avoid overriding external name by its  compute method

--- account_move_line_accounting_description_stock/models/account_move_line.py
+++ account_move_line_accounting_description_stock/models/account_move_line.py
@@ -1,7 +1,24 @@
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
 
-    external_name = fields.Text()
+  
+    @api.model_create_multi
+    def create(self, vals_list):
+        """ Only with this override the original behviour of 
+            OCA module (computing external_name) is preserved and
+            external_name is not overriden if created from sale order
+        """
+        pending = {}
+        for i, vals in enumerate(vals_list):
+            if vals.get("external_name") and vals.get("product_id"):
+                pending[i] = vals.pop("external_name")
+
+        lines = super().create(vals_list)
+
+        for i, value in pending.items():
+            lines[i].external_name = value
+
+        return lines
